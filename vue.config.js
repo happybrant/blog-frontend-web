6037dd050650e478c36e31fc532ff42f46f11fb9
scurry <2393990703@qq.com>
2024-04-29 16:29:58 +0800

    修改logo和网站标题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+ 
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false  //关闭语法检查
+  lintOnSave:false,  //关闭语法检查
+  // ...
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= '让灵魂去流浪'
+        return args
+      })
+  }
 })
